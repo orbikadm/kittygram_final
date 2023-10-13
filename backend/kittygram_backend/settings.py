4a6f0b1262f50a0c6cb28584265f0cd0389359e2
Константин Упоров <orbikadm@gmail.com>
2023-10-14 01:18:16 +1000

    allowed_ hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = env('DEBUG', default='False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
